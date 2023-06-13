b20794f50f37074b54351a4f16e969a04ab5306f
yobinmok <dyqls1214@gmail.com>
2023-06-13 18:48:25 +0900

    ✨ Custom ToolBar 추가

--- presentation/src/main/java/com/tripbook/tripbook/MainActivity.kt
+++ presentation/src/main/java/com/tripbook/tripbook/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.tripbook.tripbook
 
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.NavHost
-import androidx.fragment.app.Fragment
-import com.tripbook.tripbook.databinding.ActivityMainBinding
-import com.auth0.android.Auth0
-import com.tripbook.auth.loginWithBrowser
-import com.tripbook.auth.logout
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.tripbook.base.BaseActivity
+import com.tripbook.tripbook.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-//    private lateinit var account: Auth0
+    //    private lateinit var account: Auth0
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun init() {
 //        account = Auth0(
@@ -22,8 +22,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 //            getString(com.tripbook.tripbook.libs.auth.R.string.com_auth_domain)
 //        )
 //
-        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHost
-        navController = navHost.navController
+
 //        binding.buttonLogin.setOnClickListener {
 //            loginWithBrowser(account)
 //        }
@@ -31,9 +30,17 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 //        binding.buttonLogout.setOnClickListener {
 //            logout(account)
 //        }
-    }
-//
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHost
+        val toolbar = findViewById<Toolbar>(R.id.main_toolbar)
+        navController = navHost.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            if (destination.parent!!.startDestinationId != destination.id) {
+                toolbar.setNavigationIcon(R.drawable.baseline_arrow)
+            }
+        }
     }
 }
