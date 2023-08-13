f5952f798fd5f596a6c6386d494ac0004e0183d2
wyt_cpri <83196369+junwooYeom@users.noreply.github.com>
2023-08-13 14:03:34 +0900

    💄뉴스 툴바 생성 및 화면 연동.

--- presentation/src/main/java/com/tripbook/tripbook/views/news/BannerViewPagerAdapter.kt
+++ presentation/src/main/java/com/tripbook/tripbook/views/news/BannerViewPagerAdapter.kt
@@ -1,4 +1,14 @@
 package com.tripbook.tripbook.views.news
 
-internal class BannerViewPagerAdapter {
+import android.view.View
+import androidx.viewpager.widget.PagerAdapter
+
+internal class BannerViewPagerAdapter : PagerAdapter(){
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        TODO("Not yet implemented")
+    }
 }
