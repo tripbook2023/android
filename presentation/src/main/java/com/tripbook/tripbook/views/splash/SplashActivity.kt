f0b92396f9f2484a3fe6c3f8f88fb440fdcbc407
syeon0103 <syeon0103@naver.com>
2023-09-16 22:17:49 +0900

    ✨lottie 추가, 스플래시 코드 추가 및 manifest 변경

--- presentation/src/main/java/com/tripbook/tripbook/views/splash/SplashActivity.kt
+++ presentation/src/main/java/com/tripbook/tripbook/views/splash/SplashActivity.kt
@@ -1,15 +1,28 @@
 package com.tripbook.tripbook.views.splash
 
-import com.tripbook.base.BaseActivity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.tripbook.tripbook.MainActivity
 import com.tripbook.tripbook.R
-import com.tripbook.tripbook.databinding.ActivitySplashBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SplashActivity : BaseActivity<ActivitySplashBinding>(
-    R.layout.activity_splash
-) {
-    override fun init() {
-        TODO("Not yet implemented")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        loadSplah()
+    }
+
+    private fun loadSplah() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
