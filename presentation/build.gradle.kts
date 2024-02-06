83ecb6208592a7b38357c5cf3089b30a1faf5779
wyt_cpri <83196369+junwooYeom@users.noreply.github.com>
2024-02-07 02:37:50 +0900

    🐛 Signing-Config 변경 및 Debuggable 제거.

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -44,8 +44,6 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-            signingConfig = signingConfigs.getByName("debug")
-            isDebuggable = true
         }
     }
     compileOptions {
